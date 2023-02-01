87938f6436ac4e525d15f84a1ac108e38624bd9b
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-02-01 16:08:32 +0800

    修改部分配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@
 
 const process = require('process');
 
+const production = process.env.NODE_ENV === 'production';
+
 module.exports = {
   root: true,
   env: {
@@ -14,12 +16,24 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'comma-dangle': [2, 'only-multiline'],
     '@typescript-eslint/comma-dangle': [2, 'always-multiline'],
-    'curly': [2, 'multi-line'],
-    'no-debugger': process.env.NODE_ENV === 'production' ? ['error'] : ['off'],
-    'quotes': [2, 'single'],
-    'semi': [2, 'always'],
+    '@typescript-eslint/object-curly-spacing': [2, 'always'],
+    '@typescript-eslint/member-delimiter-style': [2, {
+      'multiline': { 'delimiter': 'semi', 'requireLast': true },
+      'singleline': { 'delimiter': 'semi', 'requireLast': true },
+    }],
+    '@typescript-eslint/no-debugger': production ? [2] : [0],
+    '@typescript-eslint/no-floating-promises': [0],
+    '@typescript-eslint/quotes': [2, 'single'],
     '@typescript-eslint/semi': [2, 'always'],
+    '@typescript-eslint/space-before-function-paren': [2, 'never'],
+    '@typescript-eslint/strict-boolean-expressions': [2, {
+      'allowString': true,
+      'allowNumber': true,
+      'allowNullableObject': true,
+      'allowNullableBoolean': true,
+      'allowNullableString': true,
+      'allowNullableNumber': true,
+    }],
   }
 };

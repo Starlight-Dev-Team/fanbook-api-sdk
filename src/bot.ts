8f4305f5c9471ff3595ba5562871ca68affb6d7d
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-02-02 10:52:14 +0800

    完成功能

--- src/bot.ts
+++ src/bot.ts
@@ -4,7 +4,11 @@
 
 import { requester, send } from '@/util';
 
-import type { User } from './interface';
+import type {
+  FormattingOptions,
+  Message,
+  User,
+} from './interface';
 
 export interface Profile {
   /**
@@ -31,6 +35,53 @@ export interface Profile {
    * 是否未完成入门仪式。
    */
   isPending: boolean;
+}
+
+export interface SendMessageConfig {
+  /**
+   * 目标聊天。
+   */
+  chat: bigint;
+  /**
+   * 消息内容。
+   */
+  text: string;
+  /**
+   * 消息内容解析方式。
+   *
+   * 纯文本消息本项不填，否则填 `Fanbook` 。
+   */
+  parseMode?: FormattingOptions;
+  /**
+   * 是否仅发送者和@到的人可见。
+   */
+  isOnlyParticipantsCanRead?: boolean;
+  /**
+   * 所回复的消息。
+   */
+  replyTo?: bigint;
+  /**
+   * 是否不允许消息被表态。
+   */
+  isUnreactive?: boolean;
+  /**
+   * 消息简介。
+   *
+   * 纯文本，在私聊列表里显示。
+   */
+  description: string;
+  /**
+   * 是否临时消息。
+   *
+   * 临时消息不持久化、不多端同步、无法撤回，清除缓存后消失。
+   */
+  isEphemeral?: boolean;
+  /**
+   * 能收到消息的用户。
+   *
+   * 仅 `isEphemeral` 为 `true` 时有效，为空时发给所有在线用户。
+   */
+  sendTo?: bigint[];
 }
 
 /**
@@ -63,5 +114,31 @@ export class Bot {
       privacyMode: res.can_read_all_group_messages,
       isPending: false, // 机器人不需要入门仪式
     };
+  }
+
+  /**
+   * 发送消息。
+   * @param config 配置项
+   * @returns 消息 ID
+   */
+  public async sendMessage(config: SendMessageConfig): Promise<bigint> {
+    const data = {
+      chat_id: config.chat,
+      text: config.text,
+      parse_mode: config.parseMode,
+      selective: config.isOnlyParticipantsCanRead,
+      reply_to_message_id: config.replyTo,
+      unreactive: config.isUnreactive ? 1 : 0,
+      desc: config.description,
+      ephemeral: config.isEphemeral,
+      // 如果 `isEphemeral` 为空，则不传入此项。
+      // 否则，此项默认为 `['all']`
+      users: config.isEphemeral ? (config.sendTo ?? ['all']) : undefined,
+    };
+    const res: Message = await send(requester.post(
+      `${this.publicPath}/sendMessage`,
+      data,
+    ));
+    return res.message_id;
   }
 }

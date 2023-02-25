8352d08144d7a8608b628b8ed4cb152c58b1a9aa
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-02-25 09:20:44 +0800

    添加示例

---
+++ examples/get-user.ts
@@ -0,0 +1,18 @@
+/**
+ * 通过 Fanbook # 获取用户 ID 示例。
+ */
+
+import * as fanbook from '@starlight-dev-team/fanbook-api-sdk';
+
+const token = ''; // 机器人令牌
+const guild = ''; // 服务器 ID （字符串形式）
+const id = 995858; // 用户 Fanbook #
+
+const bot = new fanbook.Bot(token);
+
+async function main() {
+  const channels = await bot.getUserByShortId(BigInt(guild), id);
+  console.log('用户 ID =', channels);
+}
+
+main();

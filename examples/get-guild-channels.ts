8d615c7834c1c1cd8432ac0c39a38fd3be5588ca
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-02-25 08:59:02 +0800

    添加示例

---
+++ examples/get-guild-channels.ts
@@ -0,0 +1,17 @@
+/**
+ * 获取服务器频道列表示例。
+ */
+
+import * as fanbook from '@starlight-dev-team/fanbook-api-sdk';
+
+const token = ''; // 机器人令牌
+const guild = ''; // 服务器 ID （字符串形式）
+
+const bot = new fanbook.Bot(token);
+
+async function main() {
+  const channels = await bot.getGuildChannels(BigInt(guild));
+  console.log('频道列表 = ', channels);
+}
+
+main();

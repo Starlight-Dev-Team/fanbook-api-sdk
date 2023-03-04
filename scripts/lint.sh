e560b4f12fd3d63b641992776067010eb6613ffc
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-03-04 15:07:15 +0800

    Lint 时安装依赖

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,2 +1,6 @@
 npm install -g eslint;
+yarn install eslint-config-standard-with-typescript
+yarn install eslint-plugin-import
+yarn install eslint-plugin-n
+yarn install eslint-plugin-promise
 eslint ./src;

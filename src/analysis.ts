bb326949df9d94d633cddb44ba5d9f579f4a3149
KaiKai <kaikai_miniworld@outlook.com>
2023-03-06 22:12:58 +0800

    添加“设置卡槽（荣誉）数据”方法 (#50)

---
+++ src/analysis.ts
@@ -0,0 +1,34 @@
+/**
+ * 自定义数据类型转换为原生 API 数据类型。
+ */
+
+import type * as native from '@/interface';
+import type * as types from '@/types';
+
+export function guildCredit(data: types.GuildCredit): {
+  id: string;
+  credit: native.GuildCredit;
+} {
+  const slots: native.GuildCredit['slots'] = [];
+  for (const row of data.slots) {
+    const res: native.GuildCredit['slots'][0] = [];
+    for (const { image, label, value } of row) {
+      res.push({
+        img: image,
+        label,
+        value,
+      });
+    }
+    slots.push(res);
+  }
+  return {
+    id: data.id,
+    credit: {
+      authority: data.authority,
+      slots,
+      title: {
+        img: data.title.icon,
+      },
+    },
+  };
+}
